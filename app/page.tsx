1d261cc07a087f72b081797dd0de74dd485746e9
afifvdin <afifudin.fdn@gmail.com>
2024-07-14 22:20:24 +0700

    personal website revamp

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,176 @@
-import Image from "next/image";
+import { ThemeToggle } from "@/components/theme-toggle"
+import { ArrowUpRight } from "lucide-react"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="h-full font-medium dark:font-normal bg-neutral-100 dark:bg-neutral-950 flex flex-col items-center justify-center">
+      <div className="w-full overflow-y-auto">
+        <div className="max-w-2xl mx-auto px-4 py-8">
+          <div className="flex items-center justify-between gap-4">
+            <h1 className="text-xl tracking-tight">Afifudin Abdullah</h1>
+            <ThemeToggle />
+          </div>
+          <br />
+          <p className="text-foreground/75">
+            <span>Recently graduated with degree in computer science. </span>
+            <span>I love building things and solving problems. </span>
+            <span>I enjoy web development and app development. </span>
+          </p>
+          <br />
+          <p className="text-foreground/75">
+            Sometimes, when it&apos;s not about an app, it&apos;s about AI or
+            deep learning.
+          </p>
+          <br />
+          <br />
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-6">
+            <div>
+              <h2 className="text-sm text-foreground/50 mb-4">Building</h2>
+              <h3 className="underline mb-2">
+                <Link href="https://yer.afifvdin.dev" target="_blank">
+                  Yer
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                <span>
+                  An app that helps developers to make Part-of-Speech Tagging
+                  label for deep learning dataset purpose.
+                </span>
+                <br />
+                <span className="text-xs text-foreground/50">
+                  Next.js with TailwindCSS.
+                </span>
+              </p>
+              <br />
+              <h3 className="underline mb-2">
+                <Link href="https://tagger.afifvdin.dev" target="_blank">
+                  Tagger
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                <span>
+                  My skripsi app for Part-of-Speech Tagging on X (Twitter).
+                </span>
+                <br />
+                <span className="text-xs text-foreground/50">
+                  Next.js, TailwindCSS and Python.
+                </span>
+              </p>
+            </div>
+            <div>
+              <h2 className="text-sm text-foreground/50 mb-4">Projects</h2>
+              <h3 className="underline mb-2">
+                <Link href="https://luct.afifvdin.dev" target="_blank">
+                  Luct
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                <span>
+                  An app to segment lung area using digital CT-scan images.
+                </span>
+                <br />
+                <span className="text-xs text-foreground/50">
+                  Next.js, TailwindCSS, Flutter, and Python (GCP).
+                </span>
+              </p>
+              <br />
+              <h3 className="underline mb-2">
+                <Link href="https://unvolds.afifvdin.dev" target="_blank">
+                  Unvolds
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                <span>
+                  An app to detect COVID-19 using digital X-ray images.
+                </span>
+                <br />
+                <span className="text-xs text-foreground/50">
+                  Next.js, TailwindCSS, Flutter, and Python (GCP).
+                </span>
+              </p>
+              <br />
+              <h3 className="underline mb-2 text-foreground/70">
+                <Link
+                  href="https://github.com/afifvdin/dovtron"
+                  target="_blank"
+                >
+                  Dovtron (removed by Google Play)
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                <span>An app to detect corn leaves disease.</span>
+                <br />
+                <span className="text-xs text-foreground/50">
+                  Flutter and Python (GCP).
+                </span>
+              </p>
+            </div>
+            <div>
+              <h2 className="text-sm text-foreground/50 mb-4">Open Source</h2>
+              <h3 className="underline mb-2">
+                <Link
+                  href="https://www.npmjs.com/package/@cspell/dict-id-id"
+                  target="_blank"
+                >
+                  Code Spell
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                Creating Indonesian dictionary for Spell Checker.
+              </p>
+              <br />
+              <h3 className="underline mb-2">
+                <Link
+                  href="https://marketplace.visualstudio.com/items?itemName=streetsidesoftware.code-spell-checker-indonesian"
+                  target="_blank"
+                >
+                  VS Code Code Spell Dictionary Extension
+                </Link>
+              </h3>
+              <p className="text-foreground/75">
+                Bringing the Indonesian Spell Checker into Visual Studio Code
+                Extension.
+              </p>
+            </div>
+          </div>
+          <br />
+          <br />
+          <div className="flex items-center gap-4 flex-wrap">
+            <Link
+              href="mailto:afifudin.fdn@gmail.com"
+              className="flex items-center gap-1 justify-center text-foreground/50 hover:text-foreground transition-all"
+            >
+              <span>Google Mail</span>
+              <ArrowUpRight className="size-4" />
+            </Link>
+            <Link
+              href="https://linkedin.com/in/afifudin"
+              target="_blank"
+              className="flex items-center gap-1 justify-center text-foreground/50 hover:text-foreground transition-all"
+            >
+              <span>LinkedIn</span>
+              <ArrowUpRight className="size-4" />
+            </Link>
+            <Link
+              href="https://github.com/afifvdin"
+              target="_blank"
+              className="flex items-center gap-1 justify-center text-foreground/50 hover:text-foreground transition-all"
+            >
+              <span>Github</span>
+              <ArrowUpRight className="size-4" />
+            </Link>
+            <Link
+              href="https://x.com/afifvdin"
+              target="_blank"
+              className="flex items-center gap-1 justify-center text-foreground/50 hover:text-foreground transition-all"
+            >
+              <span>X (Twitter)</span>
+              <ArrowUpRight className="size-4" />
+            </Link>
+          </div>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
