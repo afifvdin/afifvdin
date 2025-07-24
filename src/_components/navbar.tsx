a25dfd287f47665054743cce28ab31c604e9363c
afifvdin <afifudin.fdn@gmail.com>
2025-07-24 20:29:39 +0700

    update navbar and smooth scrolling

--- src/_components/navbar.tsx
+++ src/_components/navbar.tsx
@@ -4,20 +4,20 @@ export default function Navbar() {
   return (
     <nav className="flex items-center justify-between py-4">
       <ul className="flex items-center gap-4 tracking-tight sm:gap-8">
-        <li>
+        <li className="hidden sm:block">
           <img
             src="https://avatars.githubusercontent.com/u/94339143?v=4"
             className="size-7 rounded-full"
             alt="afifvdin"
           />
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer underline">
           <a href="#work-experiences">work experiences</a>
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer underline">
           <a href="#projects">projects</a>
         </li>
-        <li className="cursor-pointer">
+        <li className="cursor-pointer underline">
           <a href="#open-source">open source</a>
         </li>
       </ul>
